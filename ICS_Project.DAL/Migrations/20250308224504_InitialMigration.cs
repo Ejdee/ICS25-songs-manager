d698367bd585e920f320eec2ba87d3a21570e518
Adam Behoun <adam.behoun@seznam.cz>
2025-03-09 01:23:36 +0100

    feat(Migrations): add initial migration

---
+++ ICS_Project.DAL/Migrations/20250308224504_InitialMigration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ICS_Project.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Playlists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Genre = table.Column<string>(type: "TEXT", nullable: false),
+                    Size = table.Column<float>(type: "REAL", nullable: false),
+                    DurationInSeconds = table.Column<int>(type: "INTEGER", nullable: false),
+                    Artist = table.Column<string>(type: "TEXT", nullable: false),
+                    SongUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    PlaylistEntityId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Songs_Playlists_PlaylistEntityId",
+                        column: x => x.PlaylistEntityId,
+                        principalTable: "Playlists",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_PlaylistEntityId",
+                table: "Songs",
+                column: "PlaylistEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Playlists");
+        }
+    }
+}
