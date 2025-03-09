4ad5cccc1d14b3f10f3719915016f3b0bbb05878
Adam Behoun <adam.behoun@seznam.cz>
2025-03-09 01:21:41 +0100

    feat(DbContext): update the relationships after the artist and album removal

--- ICS_Project.DAL/ICSDbContext.cs
+++ ICS_Project.DAL/ICSDbContext.cs
@@ -5,7 +5,5 @@ namespace ICS_Project.DAL;
 public class IcsDbContext(DbContextOptions options): DbContext(options) 
 {
     public DbSet<SongEntity> Songs { get; set; }
-    public DbSet<ArtistEntity> Artists { get; set; }
-    public DbSet<AlbumEntity> Albums { get; set; }
     public DbSet<PlaylistEntity> Playlists { get; set; }
 }
