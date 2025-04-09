99adf4b29dbb51b9a4893b82e6bf806ca34d1523
Alexandra Pacalajová <alexandrapacalajova27@gmail.com>
2025-04-09 16:54:13 +0200

    fix: add IEnumerable MapToListModel

--- ICS_Project.BL/Mappers/ModelMapperBase.cs
+++ ICS_Project.BL/Mappers/ModelMapperBase.cs
@@ -5,6 +5,9 @@ namespace ICS_Project.BL.Mappers;
 public abstract class ModelMapperBase<TEntity, TListModel, TDetailModel> : IModelMapper<TEntity, TListModel, TDetailModel>
 {
     public abstract TListModel MapToListModel(TEntity? entity);
+    
+    public IEnumerable<TListModel> MapToListModel(IEnumerable<TEntity> entities) 
+        => entities.Select(MapToListModel);
 
     public abstract TDetailModel MapToDetailModel(TEntity? entity);
 
