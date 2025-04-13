f66e42758dde316d3c11d7de072df54f4b1f8197
Adam Behoun <adam.behoun@seznam.cz>
2025-04-13 12:18:05 +0200

    feat: add more BL mappers tests

--- ICS_Project.BL.Tests/BlMappersTests.cs
+++ ICS_Project.BL.Tests/BlMappersTests.cs
@@ -1,3 +1,4 @@
+using ICS_Project.BL.Mappers;
 using ICS_Project.Common.Tests.Seeds;
 using ICS_Project.DAL.Entities;
 using Xunit.Abstractions;
@@ -12,23 +13,15 @@ public class BlMappersTests : FacadeTestBase
     }
 
     [Fact]
-    public void MapToListModel_Songs_IEnumerable()
+    public void MapToListModel_PlaylistSong_NullInput_ReturnsEmpty()
     {
-        //Arrange
-        var songList = new List<SongEntity>
-        {
-            SongSeeds.BillieJean,
-            SongSeeds.ShapeOfYou,
-            SongSeeds.SongZ
-        };
-        
         //Act
-        var mappedList = SongModelMapper.MapToListModel(songList);
+        var listModel = PlaylistSongModelMapper.MapToListModel((PlaylistSongEntity)null!);
         
         //Assert
-        Assert.NotNull(mappedList);
-        Assert.Equal(songList.Count, mappedList.Count());
-    } 
+        Assert.Equal(string.Empty, listModel.SongName);
+        Assert.Equal(TimeSpan.Zero, listModel.SongDurationInSeconds);
+    }
 
     [Fact]
     public void MapToDetailModel_PlaylistSong_GetValues()
@@ -71,5 +64,225 @@ public class BlMappersTests : FacadeTestBase
         {
             PlaylistSongModelMapper.MapToEntity(detailModel);
         });
+    }
+    
+    [Fact]
+    public void MapToDetailModel_Song_GetValues()
+    {
+        //Arrange
+        var song = SongSeeds.BillieJean;
+    
+        //Act
+        var detailModel = SongModelMapper.MapToDetailModel(song);
+    
+        //Assert
+        Assert.Equal(song.Id, detailModel.Id);
+        Assert.Equal(song.Name, detailModel.Name);
+        Assert.Equal(song.Description, detailModel.Description);
+        Assert.Equal(song.DurationInSeconds, detailModel.DurationInSeconds.TotalSeconds);
+        Assert.Equal(song.Genre, detailModel.Genre);
+        Assert.Equal(song.Artist, detailModel.Artist);
+        Assert.Equal(song.SongUrl, detailModel.SongUrl);
+    }
+    
+    [Fact]
+    public void MapToDetailModel_Song_NullInput_ReturnsEmpty()
+    {
+        //Act
+        var detailModel = SongModelMapper.MapToDetailModel(null);
+    
+        //Assert
+        Assert.Equal(string.Empty, detailModel.Name);
+        Assert.Equal(TimeSpan.Zero, detailModel.DurationInSeconds);
+        Assert.Equal(string.Empty, detailModel.Description);
+        Assert.Equal(string.Empty, detailModel.Genre);
+        Assert.Equal(string.Empty, detailModel.Artist);
+        Assert.Equal(string.Empty, detailModel.SongUrl);
+    }
+    
+    [Fact]
+    public void MapToListModel_Song_NullInput_ReturnsEmpty()
+    {
+        //Act
+        var listModel = SongModelMapper.MapToListModel((SongEntity)null!);
+    
+        //Assert
+        Assert.Equal(string.Empty, listModel.Name);
+        Assert.Equal(TimeSpan.Zero, listModel.DurationInSeconds);
+    }
+    
+    [Fact]
+    public void MapToListModel_Songs_IEnumerable()
+    {
+        //Arrange
+        var songList = new List<SongEntity>
+        {
+            SongSeeds.BillieJean,
+            SongSeeds.ShapeOfYou,
+            SongSeeds.SongZ
+        };
+        
+        //Act
+        var mappedList = SongModelMapper.MapToListModel(songList);
+        
+        //Assert
+        Assert.NotNull(mappedList);
+        Assert.Equal(songList.Count, mappedList.Count());
+    } 
+    
+    [Fact]
+    public void MapToListModel_Song_GetValues()
+    {
+        //Arrange
+        var song = SongSeeds.ShapeOfYou;
+    
+        //Act
+        var listModel = SongModelMapper.MapToListModel(song);
+    
+        //Assert
+        Assert.Equal(song.Id, listModel.Id);
+        Assert.Equal(song.Name, listModel.Name);
+        Assert.Equal(song.DurationInSeconds, listModel.DurationInSeconds.TotalSeconds);
+    }
+
+    [Fact]
+    public void MapDetailToListModel_Song_GetValues()
+    {
+        //Arrange
+        var detailModel = SongModelMapper.MapToDetailModel(SongSeeds.BillieJean);
+    
+        //Act
+        var listModel = SongModelMapper.MapToListModel(SongModelMapper.MapToEntity(detailModel));
+    
+        //Assert
+        Assert.Equal(detailModel.Id, listModel.Id);
+        Assert.Equal(detailModel.Name, listModel.Name);
+        Assert.Equal(detailModel.DurationInSeconds, listModel.DurationInSeconds);
+    }
+    
+    [Fact]
+    public void MapToEntity_Song_GetValues()
+    {
+        //Arrange
+        var detailModel = SongModelMapper.MapToDetailModel(SongSeeds.ShapeOfYou);
+    
+        //Act
+        var entity = SongModelMapper.MapToEntity(detailModel);
+    
+        //Assert
+        Assert.Equal(detailModel.Id, entity.Id);
+        Assert.Equal(detailModel.Name, entity.Name);
+        Assert.Equal(detailModel.Description, entity.Description);
+        Assert.Equal((int)detailModel.DurationInSeconds.TotalSeconds, entity.DurationInSeconds);
+        Assert.Equal(detailModel.Genre, entity.Genre);
+        Assert.Equal(detailModel.Artist, entity.Artist);
+        Assert.Equal(detailModel.SongUrl, entity.SongUrl);
+    }
+    
+    [Fact]
+    public void MapToListModel_Playlist_GetValues()
+    {
+        //Arrange
+        var playlist = PlaylistSeeds.PlaylistA;
+        var mapper = new PlaylistModelMapper(new PlaylistSongModelMapper());
+        
+        //Act
+        var listModel = mapper.MapToListModel(playlist);
+        
+        //Assert
+        Assert.Equal(playlist.Id, listModel.Id);
+        Assert.Equal(playlist.Name, listModel.Name);
+        Assert.Equal(TimeSpan.FromSeconds(playlist.PlaylistSongs.Sum(x => x.Song.DurationInSeconds)), listModel.DurationInSeconds);
+        Assert.Equal(playlist.PlaylistSongs.Count, listModel.SongCount);
+    }
+    
+    [Fact]
+    public void MapToListModel_Playlist_NullInput_ReturnsEmpty()
+    {
+        //Arrange
+        var mapper = new PlaylistModelMapper(new PlaylistSongModelMapper());
+        
+        //Act
+        var listModel = mapper.MapToListModel((PlaylistEntity)null!);
+        
+        //Assert
+        Assert.Equal(string.Empty, listModel.Name);
+        Assert.Equal(TimeSpan.Zero, listModel.DurationInSeconds);
+        Assert.Equal(0, listModel.SongCount);
+    }
+    
+    [Fact]
+    public void MapToDetailModel_Playlist_GetValues()
+    {
+        //Arrange
+        var playlist = PlaylistSeeds.PlaylistA;
+        var mapper = new PlaylistModelMapper(new PlaylistSongModelMapper());
+        
+        //Act
+        var detailModel = mapper.MapToDetailModel(playlist);
+        
+        //Assert
+        Assert.Equal(playlist.Id, detailModel.Id);
+        Assert.Equal(playlist.Name, detailModel.Name);
+        Assert.Equal(playlist.Description, detailModel.Description);
+        Assert.Equal(TimeSpan.FromSeconds(playlist.PlaylistSongs.Sum(x => x.Song.DurationInSeconds)), detailModel.DurationInSeconds);
+        Assert.Equal(playlist.PlaylistSongs.Count, detailModel.SongCount);
+        Assert.NotNull(detailModel.Songs);
+        Assert.Equal(playlist.PlaylistSongs.Count, detailModel.Songs.Count);
+    }
+    
+    [Fact]
+    public void MapToDetailModel_Playlist_NullInput_ReturnsEmpty()
+    {
+        //Arrange
+        var mapper = new PlaylistModelMapper(new PlaylistSongModelMapper());
+        
+        //Act
+        var detailModel = mapper.MapToDetailModel(null);
+        
+        //Assert
+        Assert.Equal(string.Empty, detailModel.Name);
+        Assert.Equal(string.Empty, detailModel.Description);
+        Assert.Equal(TimeSpan.Zero, detailModel.DurationInSeconds);
+        Assert.Equal(0, detailModel.SongCount);
+        Assert.NotNull(detailModel.Songs);
+        Assert.Empty(detailModel.Songs);
+    }
+    
+    [Fact]
+    public void MapToEntity_Playlist_GetValues()
+    {
+        //Arrange
+        var playlist = PlaylistSeeds.PlaylistA;
+        var mapper = new PlaylistModelMapper(new PlaylistSongModelMapper());
+        var detailModel = mapper.MapToDetailModel(playlist);
+        
+        //Act
+        var entity = mapper.MapToEntity(detailModel);
+        
+        //Assert
+        Assert.Equal(detailModel.Id, entity.Id);
+        Assert.Equal(detailModel.Name, entity.Name);
+        Assert.Equal(detailModel.Description, entity.Description);
+        // Note: PlaylistSongs are not mapped in MapToEntity
+    }
+    
+    [Fact]
+    public void MapToListModels_Playlists_IEnumerable()
+    {
+        //Arrange
+        var playlists = new List<PlaylistEntity>
+        {
+            PlaylistSeeds.PlaylistA,
+            PlaylistSeeds.PlaylistB
+        };
+        var mapper = new PlaylistModelMapper(new PlaylistSongModelMapper());
+        
+        //Act
+        var mappedList = mapper.MapToListModel(playlists);
+        
+        //Assert
+        Assert.NotNull(mappedList);
+        Assert.Equal(playlists.Count, mappedList.Count());
     }
 }
