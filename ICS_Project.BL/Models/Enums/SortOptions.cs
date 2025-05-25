28f37929bdb313864b3c1e8baedce2bd70832bf1
Adam Behoun <adam.behoun@seznam.cz>
2025-05-25 23:41:48 +0200

    feat: convert names of filters from enum

--- ICS_Project.BL/Models/Enums/SortOptions.cs
+++ ICS_Project.BL/Models/Enums/SortOptions.cs
@@ -1,11 +1,19 @@
-﻿namespace ICS_Project.BL.Models.Enums;
+﻿using System.ComponentModel;
+
+namespace ICS_Project.BL.Models.Enums;
 
 public enum SortOptions
 {
+    [Description("Song Name")]
     SongName,
+    
+    [Description("Song Duration")]
     SongDuration,
     
+    [Description("Playlist Name")]
     PlaylistName,
+    [Description("Song Count")]
     PlaylistSongCount,
+    [Description("Playlist Duration")]
     PlaylistDuration,
 }
